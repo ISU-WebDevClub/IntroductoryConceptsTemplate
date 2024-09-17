d4c7b845b7bb171bda578e83b5513dd66c01607f
Noam Hazan <noam.hazan01@gmail.com>
2024-09-17 18:48:46 -0500

    noam changes

--- code/main.js
+++ code/main.js
@@ -1,11 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("Page has loaded");
+// document.addEventListener("DOMContentLoaded", () => {
+//     alert("Page has loaded");
 
-    let clickableHeader = document.getElementById("clickable");
-    if(clickableHeader != null) {
-        alert("found header")
-    }
-    clickableHeader.addEventListener("click", () => {
-        alert("Clickable Element has been clicked!");
-    })
-});
+//     let clickableHeader = document.getElementById("clickable");
+//     if(clickableHeader != null) {
+//         alert("found header")
+//     }
+//     clickableHeader.addEventListener("click", () => {
+//         alert("Clickable Element has been clicked!");
+//     })
+// });
+
+
+var confettiColors = ["#4986be", "#6946ae", "#A996ae"]
+
+function targetedConfetti(){
+    var target = document.getElementById("confetti").getBoundingClientRect()
+    var screen = document.body.getBoundingClientRect()
+    var xPos = (((target.left+target.right)/2)/window.innerWidth)
+    var yPos = (((target.top+target.bottom)/2)/window.innerHeight)
+    confetti({
+        particleCount: 250,
+        startVelocity: 30,
+        spread: 1000,
+        colors: confettiColors,
+        ticks: 200,
+        zIndex: 3,
+        origin: {
+        x: xPos.toFixed(3),
+        y: yPos.toFixed(3)
+        }
+    });
+}
